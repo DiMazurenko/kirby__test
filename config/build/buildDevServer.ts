5b5cdec143acc00076de84b47f4f7a1d9016fe91
Dmitriy <dimazurenko@gmail.com>
2023-10-12 15:17:21 +0300

    add webpack hot module

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,6 +6,8 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
         port: options.port,
         open: true,
         historyApiFallback: true,
+        hot: true,
+        
     };
 }
 
