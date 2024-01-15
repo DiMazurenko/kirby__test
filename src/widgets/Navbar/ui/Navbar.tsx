03de2977868338aff1addf7a48010d39d9b537c5
Dmitriy <dimazurenko@gmail.com>
2024-01-15 17:42:18 +0300

    add LoginModal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,34 +1,37 @@
-import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './Navbar.module.scss'
-import { Modal } from 'shared/ui/Modal/Modal'
-import React, { useCallback, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Button, ButtonTheme } from 'shared/ui/Button/Button'
+import {classNames} from 'shared/lib/classNames/classNames';
+import cls from './Navbar.module.scss';
+import {Modal} from 'shared/ui/Modal/Modal';
+import React, {useCallback, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {Button, ButtonTheme} from 'shared/ui/Button/Button';
+import {LoginModal} from 'features/AuthByUsername';
 
 interface NavbarProps {
-  className?: string
+	className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation()
-  const [isAuthModal, setIsAuthModal] = useState(false)
+export const Navbar = ({className}: NavbarProps) => {
+	const {t} = useTranslation();
+	const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev)
-  }, [])
+	const onCloseModal = useCallback(() => {
+		setIsAuthModal(false);
+	}, []);
 
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <Button
-        theme={ButtonTheme.CLEAR_INVERTED}
-        className={classNames(cls.links, {}, [])}
-        onClick={onToggleModal}
-      >
-        {t('Войти')}
-      </Button>
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        <p>HI</p>
-      </Modal>
-    </div>
-  )
-}
+	const onShowModal = useCallback(() => {
+		setIsAuthModal(true);
+	}, []);
+
+	return (
+		<div className={classNames(cls.Navbar, {}, [className])}>
+			<Button
+				theme={ButtonTheme.CLEAR_INVERTED}
+				className={classNames(cls.links, {}, [])}
+				onClick={onShowModal}
+			>
+				{t('Войти')}
+			</Button>
+			<LoginModal isOpen={isAuthModal} onClose={onCloseModal}/>
+		</div>
+	);
+};
