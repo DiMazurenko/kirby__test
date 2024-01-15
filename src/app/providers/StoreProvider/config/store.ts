9491f0affddb787da4fbf6fb3d4c70a19071a3cc
Dmitriy <dimazurenko@gmail.com>
2024-01-15 17:11:02 +0300

    add userSlice

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,13 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { type StateSchema } from './StateSchema'
-import { counterReducer } from 'entities/Counter'
+import {configureStore, ReducersMapObject} from '@reduxjs/toolkit';
+import {type StateSchema} from './StateSchema';
+import {counterReducer} from 'entities/Counter';
+import {userReducer} from 'entities/User';
 
-export function createReduxStore (initialState?: StateSchema) {
-  return configureStore<StateSchema>({
-    reducer: {
-      counter: counterReducer
-    },
-    devTools: __IS_DEV__,
-    preloadedState: initialState
-  })
+export function createReduxStore(initialState?: StateSchema) {
+	const rootReducers: ReducersMapObject<StateSchema> = {
+		counter: counterReducer,
+		user: userReducer
+	};
+
+	return configureStore<StateSchema>({
+		reducer: rootReducers,
+		devTools: __IS_DEV__,
+		preloadedState: initialState
+	});
 }
