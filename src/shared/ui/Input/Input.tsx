e3810a2119434deed8c1b1751a649f48a1638e10
Dmitriy <dimazurenko@gmail.com>
2024-01-23 16:57:28 +0300

    update Input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,6 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Input.module.scss';
-import React, {InputHTMLAttributes, memo, useState} from 'react';
+import React, {InputHTMLAttributes, memo, useEffect, useRef, useState} from 'react';
 
 type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
 
@@ -8,6 +8,7 @@ interface InputProps extends HTMLInputProps {
 	className?: string;
 	value?: string;
 	onChange?: (value: string) => void;
+	autofocus?: boolean;
 }
 
 export const Input = memo((props: InputProps) => {
@@ -17,11 +18,21 @@ export const Input = memo((props: InputProps) => {
 		onChange,
 		type = 'text',
 		placeholder,
+		autofocus,
 		...otherProps
 	} = props;
 
+	const ref = useRef<HTMLInputElement>(null);
+
 	const [isFocused, setIsFocused] = useState(false);
 	const [caretPosition, setCaretPosition] = useState(0);
+
+	useEffect(() => {
+		if (autofocus) {
+			setIsFocused(true);
+			ref?.current?.focus();
+		}
+	}, [autofocus]);
 
 	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		onChange?.(e.target.value);
@@ -36,7 +47,8 @@ export const Input = memo((props: InputProps) => {
 		setIsFocused(true);
 	};
 
-	const onSelect = () => {
+	const onSelect = (e: any) => {
+		setCaretPosition(e?.target?.selectionStart || 0);
 	};
 
 	return (
@@ -47,6 +59,7 @@ export const Input = memo((props: InputProps) => {
 				</div>)}
 			<div className={cls.caretWrapper}>
 				<input
+					ref={ref}
 					type={type}
 					value={value}
 					onChange={onChangeHandler}
@@ -54,6 +67,7 @@ export const Input = memo((props: InputProps) => {
 					onFocus={onFocus}
 					onBlur={onBlur}
 					onSelect={onSelect}
+					{...otherProps}
 				/>
 				{isFocused &&
 					(<span
