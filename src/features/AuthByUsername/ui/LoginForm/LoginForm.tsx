31fe084565915a72f2ba74a3ef86ff3c21cce9d8
Dmitriy <dimazurenko@gmail.com>
2024-01-25 12:34:22 +0300

    add loginState

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,34 +1,62 @@
-import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './LoginForm.module.scss'
-import { useTranslation } from 'react-i18next'
-import { Button } from 'shared/ui/Button/Button'
-import { Input } from 'shared/ui/Input/Input'
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './LoginForm.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Input } from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
 
 interface LoginFormProps {
   className?: string
 
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
-  const { t } = useTranslation()
+// eslint-disable-next-line react/display-name
+export const LoginForm = memo(({ className }: LoginFormProps) => {
+  const { t } = useTranslation();
+
+  const dispatch = useDispatch();
+  const { username, password } = useSelector(getLoginState);
+
+  const onChangeUserName = useCallback((value: string) => {
+    dispatch(loginActions.setUserName(value));
+  }, [dispatch]);
+
+  const onChangePassword = useCallback((value: string) => {
+    dispatch(loginActions.setPassword(value));
+  }, [dispatch]);
+
+  const onLoginClick = useCallback(() => {
+    //
+  }, []);
 
   return (
-        <div className={classNames(cls.LoginForm, {}, [className])}>
-          <Input
-                autofocus
-                type="text"
-                className={cls.input}
-                placeholder={t('Введите username')}
-            />
-          <Input
-                type="text"
-                className={cls.input}
-                placeholder={t('Введите пароль')}
-            />
-          <Button className={cls.loginBtn}>
-                {t('Войти')}
-            </Button>
-      </div>
+		<div className={classNames(cls.LoginForm, {}, [className])}>
+			<Input
+				autofocus
+				type="text"
+				className={cls.input}
+				placeholder={t('Введите username')}
+				onChange={onChangeUserName}
+				value={username}
+			/>
+			<Input
+				type="text"
+				className={cls.input}
+				placeholder={t('Введите пароль')}
+				onChange={onChangePassword}
+				value={password}
+			/>
+			<Button
+				className={cls.loginBtn}
+				theme={ButtonTheme.OUTLINE}
+				onClick={onLoginClick}
+			>
+				{t('Войти')}
+			</Button>
+		</div>
 
-  )
-}
+  );
+});
--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './LoginModal.module.scss'
 import { Modal } from 'shared/ui/Modal/Modal'
 import { LoginForm } from '../LoginForm/LoginForm'
 
@@ -11,13 +10,13 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-        <Modal
-          className={classNames(cls.LoginModal, {}, [className])}
-          isOpen={isOpen}
-          onClose={onClose}
-          lazy
-        >
-          <LoginForm/>
-      </Modal>
+		<Modal
+			className={classNames('', {}, [className])}
+			isOpen={isOpen}
+			onClose={onClose}
+			lazy
+		>
+			<LoginForm/>
+		</Modal>
   )
 }
