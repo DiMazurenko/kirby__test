36f784fe0187b06d5a397d19be2a5166c6604e49
Dmitriy <dimazurenko@gmail.com>
2024-02-07 16:25:30 +0300

    add tests loginByUsername

--- src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.ts
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.ts
@@ -1,3 +1,4 @@
 import { type StateSchema } from 'app/providers/StoreProvider';
+import { type DeepPartial } from '@reduxjs/toolkit';
 
 export const getLoginError = (state: StateSchema) => state?.loginForm?.error;
